80f58b389fbf43b3a2aa5e51b99a37f6b3f5edda
Csingh26 <singh.chaittanya@gmail.com>
2024-04-16 06:58:38 +0530

    advCalc

---
+++ advCliCalc/calc.h
@@ -0,0 +1,9 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+
+#include <iostream>
+#include <limits>
+void res() {
+    
+}
+#endif
---
+++ advCliCalc/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "calc.h"
+
+using namespace std;
+
+int main() 
+{
+    double num1, num2;
+    char opt;
+
+    bool flag = true;
+
+    while (flag) {
+        
+    }
+    
+    return 0;
+}
